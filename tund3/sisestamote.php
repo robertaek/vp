9f0e91305909e11dabbe62096003f41f24c4599d
Roberta Elisabeth Kollo <70900277+robertaek@users.noreply.github.com>
2020-09-20 21:56:59 +0300

    Kodutöö osa (mõtete lugemine)

---
+++ tund3/sisestamote.php
@@ -0,0 +1,47 @@
+ <!-- see post method teeb nii, et url-i ei läheks see, mis vormis saadetud on-->
+   <?php  //loeme andmebaasi login info muutujad
+  require("../../../config.php");
+  //kui kasutaja on vormis andmeid saatnud, siis salvestame andmebaasi
+  $database = "if20_roberta_k_3";
+  if(isset($_POST["submitnonsens"])){
+	  if(!empty($_POST["nonsens"])){
+		  //andmebaasi lisamine
+		  //loome andmebaasi ühenduse
+		  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+		  //valmistame ette SQL käsu
+		  $stmt = $conn->prepare("INSERT INTO nonsens (nonsensidea) VALUES(?)");
+		  echo $conn->error;
+		  //s - string, i - integer, d - decimal
+		  $stmt->bind_param("s", $_POST["nonsens"]);
+		  $stmt->execute();
+		  //käsk ja ühendus sulgeda
+		  $stmt->close();
+		  $conn->close();
+	  }
+  }
+
+  //loeme andmebaasist
+  $nonsenshtml = "";
+  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+  //valmistame ette SQL käsu
+  $stmt = $conn->prepare("SELECT nonsensidea FROM nonsens");
+  echo $conn->error;
+  //seome tulemuse mingi muutujaga
+  $stmt->bind_result($nonsensfromdb);
+  $stmt->execute();
+  //võtan kuni on
+  while($stmt->fetch()){
+	  $nonsenshtml .= "<p>" .$nonsensfromdb ."</p>";
+  }
+  $stmt->close();
+  $conn->close();
+  //ongi andmebaasist loetud ?>
+  <hr>
+  <form method="POST">
+	<label>Sisesta oma tänane mõttetu mõte!</label>
+	<input type="text" name="nonsens" placeholder="mõttekoht">
+	<input type="submit" value="Saada ära!" name="submitnonsens">
+  </form>
+  <hr> 
+ 
+  
